5dbdff5cec59e52a4d96dde76bd8fdc125dc2a55
lihongmin <li89hong05min17>
2019-11-04 00:12:54 +0800

    Spring事件驱动初始化异步线程池

--- src/main/java/com/kevin/designpattern/DesignpatternApplication.java
+++ src/main/java/com/kevin/designpattern/DesignpatternApplication.java
@@ -1,8 +1,15 @@
 package com.kevin.designpattern;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+
+import java.util.concurrent.*;
 
 
 /**
@@ -13,7 +20,24 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @ComponentScan(basePackages = "com.kevin.designpattern.headfirst.observer.spring")
 @SpringBootApplication
-public class DesignpatternApplication {
+public class DesignpatternApplication implements BeanFactoryAware {
+
+	private BeanFactory beanFactory;
+
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		this.beanFactory = beanFactory;
+	}
+
+	@Bean
+	public SimpleApplicationEventMulticaster init() {
+		ThreadPoolExecutor MulticasterExecutor = new ThreadPoolExecutor(5, 5, 60, TimeUnit.SECONDS,
+				new LinkedBlockingDeque<>(), Executors.defaultThreadFactory(), new ThreadPoolExecutor.DiscardPolicy());
+		SimpleApplicationEventMulticaster multicaster = new SimpleApplicationEventMulticaster();
+		multicaster.setTaskExecutor(MulticasterExecutor);
+		multicaster.setBeanFactory(beanFactory);
+		return multicaster;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesignpatternApplication.class, args);
